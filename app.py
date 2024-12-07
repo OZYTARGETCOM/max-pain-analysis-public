b349175d9441177377c3dcde2a2b5a578ce2a831
OZYTARGETCOM <188407936+OZYTARGETCOM@users.noreply.github.com>
2024-12-07 01:08:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
-
+import matplotlib.pyplot as plt
+import matplotlib
 
 
 # Configuración inicial de la página
